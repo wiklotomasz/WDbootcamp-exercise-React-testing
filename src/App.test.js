370fe618e4701220936df9eeb3dd1c48ab0bc445
Aleksandra Krzywicka <aleksandrakrzywicka@gmail.com>
2018-05-20 15:10:40 +0200

    Add App component with tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import App from './App';
+import PlayersList from './components/PlayersList/PlayersList.js';
 
 it('renders without crashing', () => {
   shallow(<App />);
+});
+
+it('should updateplayer score', () => {
+	const appComponent = shallow(<App />);
+	const players = [{name: 'Piotr', score: 5}, {name: 'Paweł', score: 2}];
+	appComponent.setState({players});
+	const onScoreUpdate = appComponent.find(PlayersList).prop('onScoreUpdate');
+
+	onScoreUpdate(0, 7);
+
+	const playersAfterUpdate = appComponent.state('players');
+
+	expect(playersAfterUpdate[0].score).toEqual(12);
 });
